4faaa7c096aca87fcfb4186ad98b39f23d0198f4
Allen <allenmcndounger@gmail.com>
2019-12-03 10:11:16 +0300

    Calling a method and then modify within it

--- classes.js
+++ classes.js
@@ -9,10 +9,22 @@ class User {
         const fullName = `${this.firstName} ${this.lastName} is my full name.`
         return fullName;
     }
+
+    editName(editName) {
+        const name = editName.split(" ");
+        this.firstName =  name[0];
+        this.lastName = name[1];
+
+        const newName = `My new name is ${this.firstName} ${this.lastName}`;
+        return newName;
+    }
 }
 
 const john = new User('John','Anderson',25);
 // console.log(john.lastName);
 
-console.log(john.getFullName());
+// console.log(john.getFullName());
+console.log(john.editName('Allen Mwandunga'));
+
+
 
